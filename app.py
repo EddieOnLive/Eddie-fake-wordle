0807ff5cc1b84988bf74b700b138c4e28ea76aa1
EddieOnLive <lumphy22fl@gmail.com>
2022-10-31 18:43:59 -0300

    Juan

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'basededatosnombre'
+mysql = MySQL(app)
+
+
+@app.route('/')
+def Index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
